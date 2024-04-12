67feb262351b620078d11221db6432a7e20e4199
Chernikov23 <alexandrchernikov@icloud.com>
2024-04-12 07:53:44 +0300

    Added inline

--- handlers/user_commands.py
+++ handlers/user_commands.py
@@ -1,12 +1,13 @@
 from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
-
+from keyboards import *
 router = Router()
 
 
 @router.message(CommandStart())
 async def start(msg: Message):
     await msg.answer(
-        f'Hi, {msg.from_user.full_name}! I am Community translator bot. Everyone can help in developing by the link https://github.com/Chernikov23/Community-Translator-Bot! '
+        f'Hi, {msg.from_user.full_name}! I am Community translator bot. Everyone can help in developing by the link https://github.com/Chernikov23/Community-Translator-Bot! ',
+        reply_markup=inline.main
     )
